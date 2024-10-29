516b97c1ef153e2cf69f2242187006350fd91d6f
PJLB <plaraber@gmail.com>
2024-10-29 20:11:18 +0100

    Segundo Commit

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola GitHub");
+		System.out.println("Segundo Commit");
 	}
 
 }
